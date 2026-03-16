848f36dda67d7be5eb91b8ca7b4a9ed44648ecdd
dragosdrg1 <dragoslaurentiu.matei@gmail.com>
2026-03-16 11:39:40 +0200

    Lombok

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,26 +1,12 @@
+import lombok.*;
+
+@Getter @AllArgsConstructor
 public abstract class BankAccount {
     private String user;
-    private double balance;
     private String accountNumber;
 
-    public BankAccount(String user, double balance, String accountNumber) {
-        this.user = user;
-        this.balance = balance;
-        this.accountNumber = accountNumber;
-    }
-
-    public String getUser() {
-        return user;
-    }
-    public double getBalance() {
-        return balance;
-    }
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-    protected void setBalance(double balance) {
-        this.balance = balance;
-    }
+    @Setter(AccessLevel.PROTECTED)
+    private double balance;
 
     public void deposit(double amount){
         if (amount > 0){
